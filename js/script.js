3877f50e7561aabe496756b5efef3aaffdb86f17
Ramon Hilário <162522764+ramonHdev@users.noreply.github.com>
2024-04-10 03:02:59 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 
-const listaDeTarefas = []
+const listaDeTarefas = [{
+  name: 'make dinner',
+  data: '2022-12-22'
+}, {
+  name: 'wash dishes',
+  data: '2022-12-22'
+}]
 
 renderListaDeTarefa()
 
@@ -7,13 +13,16 @@ function renderListaDeTarefa() {
     let listaDeTarefasHtml = ''
 
   for (let i = 0; i < listaDeTarefas.length; i++) {
-    const tarefa = listaDeTarefas[i]
+    const tarefaObjeto = listaDeTarefas[i]
+    //const name = tarefaObjeto.name
+    //const data = tarefaObjeto.data
+    const { name , data } = tarefaObjeto
     const html = `<p>
-    ${tarefa} 
+    ${name} ${data}
     <button onclick="
      listaDeTarefas.splice(${i}, 1)
      renderListaDeTarefa()
-    ">Excluir</button>
+    " class="excluirBtn">Excluir</button>
     </p>`
     listaDeTarefasHtml += html
 }
@@ -22,14 +31,17 @@ document.querySelector('.listaDeTarefa').innerHTML = listaDeTarefasHtml
 
 function tarefas() {
  const nameInput = document.querySelector('.nameInput')
- const afazer = nameInput.value
- listaDeTarefas.push(afazer)
+ const name = nameInput.value
+
+ const dataInputElemento = document.querySelector('.data-input')
+ const data = dataInputElemento.value
+
+ listaDeTarefas.push({
+  name,
+  data})
  nameInput.value = ''
  console.log(listaDeTarefas)
  renderListaDeTarefa()
 }
-
-
-
 
 
